8ee411f2ed7b4bc5fdc4f4e3afe417c20e6bda6e
Jakub Benes <jukben@gmail.com>
2015-08-20 19:52:35 +0200

    new structure

--- server.go
+++ server.go
@@ -3,29 +3,46 @@ package main
 import (
   "github.com/go-martini/martini"
   "github.com/martini-contrib/render"
-  //"github.com/jinzhu/gorm"
+  "github.com/martini-contrib/binding"
   "./copartying_rest_go"
 )
+
+func NewCopartie(r render.Render){
+  r.JSON(200, map[string]interface{}{"hello": "world"})
+}
+
+  func newCopartieItem(r render.Render) {
+    r.JSON(200, map[string]interface{}{"hello": "world"})
+  }
+
+func GetCopartie(r render.Render){
+  r.JSON(200, map[string]interface{}{"hello": "world"})
+}
+
+func UpdateCopartie(r render.Render) {
+  r.JSON(200, map[string]interface{}{"hello": "world"})
+}
+
+func DeleteCopartie(r render.Render) {
+  r.JSON(200, map[string]interface{}{"hello": "world"})
+}
+  
+
+
+
 func main() {
+  apiRoute := "/api/v1"
+
   m := martini.Classic()
   m.Use(render.Renderer())
 
-  m.Get("/", func(r render.Render) {
-    r.JSON(200, map[string]interface{}{"hello": "world"})
+  m.Group(apiRoute + "/coparties", func(r martini.Router) {
+    r.Get("/:id&secret_token=:admin_secret_token", GetCopartie)
+    r.Post("", NewCopartie)
+    r.Post(":coparty_id/items/:id&secret_token=:user_secret_token", newCopartieItem)
+    r.Put("/:id&secret_token=:admin_secret_token", UpdateCopartie)
+    r.Delete("/:id&secret_token=:admin_secret_token", DeleteCopartie)
   })
-  m.Get("/parties", GetParties)
-  m.Get("/party", GetParty)
 
   m.Run()
-}
-
-
-func GetParties(r render.Render) {
-  p := party.Party{ID: 255, Name: "Párty", Description: "Velký popis Party"}
-  r.JSON(200, p)
-}
-
-func GetParty(r render.Render) {
-  p := party.Party{ID: 255, Name: "Jedna párty", Description: "Velký popis Party"}
-  r.JSON(200, p)
 }
