4c505c70a4ab7099880f673ce7583e1178574966
Jakub Benes <jukben@gmail.com>
2015-08-20 20:04:23 +0200

    fujky panic error

--- server.go
+++ server.go
@@ -3,21 +3,19 @@ package main
 import (
   "github.com/go-martini/martini"
   "github.com/martini-contrib/render"
-  "github.com/martini-contrib/binding"
-  "./copartying_rest_go"
+  //"github.com/martini-contrib/binding"
 )
 
 func NewCopartie(r render.Render){
   r.JSON(200, map[string]interface{}{"hello": "world"})
 }
 
-func newCopartieItem(r render.Render) {
-  r.JSON(200, map[string]interface{}{"hello": "world"})
-}
+  func newCopartieItem(r render.Render) {
+    r.JSON(200, map[string]interface{}{"hello": "world"})
+  }
 
 func GetCopartie(r render.Render){
-  p := party.Party{ID: 255, Name: "Párty", Description: "Velký popis Party"}
-  r.JSON(200, p)
+  r.JSON(200, map[string]interface{}{"hello": "world"})
 }
 
 func UpdateCopartie(r render.Render) {
@@ -27,12 +25,7 @@ func UpdateCopartie(r render.Render) {
 func DeleteCopartie(r render.Render) {
   r.JSON(200, map[string]interface{}{"hello": "world"})
 }
-
-/*
-var parties [1]party.Party
-parties[0] = party.Party{ID: 255, Name: "Párty", Description: "Velký popis Party"}
-r.JSON(200, parties)
-*/
+  
 
 
 
@@ -43,11 +36,11 @@ func main() {
   m.Use(render.Renderer())
 
   m.Group(apiRoute + "/coparties", func(r martini.Router) {
-    r.Get("/:id&secret_token=:admin_secret_token", GetCopartie)
+    r.Get("/:id", GetCopartie)
     r.Post("", NewCopartie)
-    r.Post(":coparty_id/items/:id&secret_token=:user_secret_token", newCopartieItem)
-    r.Put("/:id&secret_token=:admin_secret_token", UpdateCopartie)
-    r.Delete("/:id&secret_token=:admin_secret_token", DeleteCopartie)
+    r.Post(":coparty_id/items/:id", newCopartieItem)
+    r.Put("/:id", UpdateCopartie)
+    r.Delete("/:id", DeleteCopartie)
   })
 
   m.Run()
